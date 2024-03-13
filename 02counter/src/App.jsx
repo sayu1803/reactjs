84ba3d33c61b6240c1fcf50e5f6658c4e26fca62
sayu1803 <sayantanrabi.03@gmail.com>
2024-03-13 16:37:31 +0530

    tailwind and props

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-//learning how to use useState and
+//learning how to use useState and hooks
 
 function App() {
   //let counter =5
